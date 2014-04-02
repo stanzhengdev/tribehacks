deb08ec30c10151e3c68a4f0b421f7a2582f8e23
stanzheng <dev.stanzheng@gmail.com>
2014-04-02 16:20:31 -0400

    cool configs

---
+++ config/sample_db.php
@@ -0,0 +1,6 @@
+<?php
+date_default_timezone_set('UTC');
+//define your db, your host port, db name, and password 
+define("DB_DSN", "mysql:host=127.0.0.1;dbname=tribehacks");
+define("DB_USERNAME", "root");
+define("DB_PASSWORD", "root" );
